ddd0ed5d377067524c10a171b9cd6906ce5db738
Aditya Dash <69818451+adityadash03@users.noreply.github.com>
2020-09-22 14:24:57 -0400

    Added two option button

---
+++ TwoOptionBooleanButton/generated/ManifestTypes.d.ts
@@ -0,0 +1,11 @@
+/*
+*This is auto generated from the ControlManifest.Input.xml file
+*/
+
+// Define IInputs and IOutputs Type. They should match with ControlManifest.
+export interface IInputs {
+    TwoOptionSetAttribute: ComponentFramework.PropertyTypes.TwoOptionsProperty;
+}
+export interface IOutputs {
+    TwoOptionSetAttribute?: boolean;
+}
